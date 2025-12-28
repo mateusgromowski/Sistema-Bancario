7edfafd3d10c18944acf63163446045f341e8382
Mateus Barbosa Gromowski <mateusbarbosagromowski@gmail.com>
2025-12-28 12:38:31 -0300

    Limpeza de código.

--- src/Banco/SistemaBancario.java
+++ src/Banco/SistemaBancario.java
@@ -39,24 +39,4 @@ public class SistemaBancario {
         transacoes.add(new Transacao(tipo, dinheiro, numero));
     }
 
-    public void imprimeContas() {
-        for (Conta conta : contas) {
-            System.out.println("Nome do titular: " + conta.getTitular());
-            System.out.println("Saldo da conta: " + conta.getSaldo());
-            System.out.println("Numero da conta: " + conta.getNumero());
-        }
-    }
-
-    public void imprimeTransacoes() {
-        for (Transacao t : transacoes) {
-
-            System.out.println("Tipo da Transação: " + t.getTipoTransacao());
-            System.out.println("Dia da Transação: " + t.getDiaFormatado());
-            System.out.println("Horário da Transação: " + t.getHoraFormatada());
-            System.out.println("Valor da transação: " + t.getValor());
-            System.out.println("Numero da conta: " + t.getNumeroConta());
-            System.out.println();
-        }
-    }
-
 }
